f2bef95d95521640aef8052cd1c162305aaf4d81
LindaSz0112 <lindaszarvas@gmail.com>
2023-11-03 20:19:38 +0100

    Adding OrderingArrows component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <ToDoList></ToDoList>
+  <OrderingArrows></OrderingArrows>
 </template>
 
 <script>
 import ToDoList from "@/components/ToDoList.vue";
+import OrderingArrows from "@/components/OrderingArrows.vue";
 import "./assets/styles.css";
 
 export default {
   components: {
     ToDoList,
+    OrderingArrows,
   },
 };
 </script>
---
+++ src/components/OrderingArrows.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p><span>⬆️</span><span>⬇️</span></p>
+  </div>
+</template>
