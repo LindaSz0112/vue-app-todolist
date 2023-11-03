2ae263cec4c18005f2bf5ba2ef40362c344c9d5a
LindaSz0112 <lindaszarvas@gmail.com>
2023-11-03 19:29:27 +0100

    Added time and ampm to form and select option

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -2,12 +2,19 @@
   <div class="container">
     <h1>To-Do List</h1>
     <div class="form-div">
+      <span>Time:</span>
+      <input type="number" v-model="time" />
+      <select v-model="ampm">
+        <option value="AM">AM</option>
+        <option value="PM">PM</option>
+      </select>
+      <span>To-Do:</span>
       <input type="text" v-model="newItem" />
       <button @click="addNewItem">Add</button>
     </div>
     <ul class="itemsList">
       <li v-for="(toDoItem, index) in toDoItems" :key="index">
-        {{ toDoItem }}<span>✅</span
+        {{ toDoItem[0] }} {{ toDoItem[1] }} : {{ toDoItem[2] }}<span>✅</span
         ><a @click="deleteItem(index)" title="Cancel Item">❌</a>
       </li>
     </ul>
@@ -18,15 +25,18 @@
 export default {
   data() {
     return {
+      time: "",
+      ampm: "AM",
       newItem: "",
       toDoItems: [],
     };
   },
   methods: {
     addNewItem() {
-      if (this.newItem.trim() !== "") {
-        this.toDoItems.push(this.newItem);
-        this.newItem = "";
+      if (this.newItem.trim() !== "" && this.ampm && this.time) {
+        this.toDoItems.push([this.time, this.ampm, this.newItem]);
+        this.time = this.newItem = "";
+        this.ampm = "AM";
       }
     },
     deleteItem(index) {
