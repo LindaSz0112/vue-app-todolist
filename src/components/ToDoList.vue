b853c55016f1059082bda38d3479c3f331434ac4
LindaSz0112 <lindaszarvas@gmail.com>
2023-11-03 17:55:17 +0100

    Creating file of ToDoList component

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>To-Do List</h1>
+  <div class="form-div">
+    <input type="text" />
+    <button>Add</button>
+  </div>
+  <ul class="itemsList">
+    <li>Clean the house</li>
+    <li>Call Mum</li>
+  </ul>
+</template>
