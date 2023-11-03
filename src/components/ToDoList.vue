09a573e18813db92f57ba47b44f4b80cbdd07db6
LindaSz0112 <lindaszarvas@gmail.com>
2023-11-03 18:49:54 +0100

    Creating AddNewItem method

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,11 +1,33 @@
 <template>
-  <h1>To-Do List</h1>
-  <div class="form-div">
-    <input type="text" />
-    <button>Add</button>
+  <div class="container">
+    <h1>To-Do List</h1>
+    <div class="form-div">
+      <input type="text" v-model="newItem" />
+      <button @click="addNewItem">Add</button>
+    </div>
+    <ul class="itemsList">
+      <li v-for="(toDoItem, index) in toDoItems" :key="index">
+        {{ toDoItem }}<span>✅</span><span>❌</span>
+      </li>
+    </ul>
   </div>
-  <ul class="itemsList">
-    <li>Clean the house</li>
-    <li>Call Mum</li>
-  </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: "",
+      toDoItems: [],
+    };
+  },
+  methods: {
+    addNewItem() {
+      if (this.newItem.trim() !== "") {
+        this.toDoItems.push(this.newItem);
+        this.newItem = "";
+      }
+    },
+  },
+};
+</script>
